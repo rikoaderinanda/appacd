680357ac003ed9f192b32295730af2be3bac91f2
rikoaderinanda <rikoaderinanda@gmail.com>
2025-07-31 17:15:02 +0700

    rappiin data init gradle

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,10 @@ public class HomeController : Controller
         return View();
     }
 
-    
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
     public IActionResult Profile()
     {
